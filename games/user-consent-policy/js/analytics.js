f19ee65614c714e5f763d111dd286422a0d3351a
5Vmc <btme@qq.com>
2021-06-26 01:02:53 +0800

    EEA政策

---
+++ games/user-consent-policy/js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?65d95149b06836d8be99a0e7c66360a8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
